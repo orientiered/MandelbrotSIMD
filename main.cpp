d6880c2a0a2aa9e682c9b5e7edeaeb6f8508cc81
orientiered <da.morgachev@mail.ru>
2025-03-26 19:12:35 +0300

    Added zooming and positioning

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Config.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <stdio.h>
 
@@ -11,18 +12,28 @@ typedef struct color{
     uint8_t a;
 } color_t;
 
-int calculateMandelbrot(uint32_t *pixels, const unsigned WIDTH, const unsigned HEIGHT, const float scale);
+typedef struct {
+    sf::Vector2f oldPosition;
+    bool pressed;
+} mouseState_t;
 
-const float MANDELBROT_WIDTH  = 3.0;
-float MANDELBROT_HEIGHT = 2.0;
+int calculateMandelbrot(uint32_t *pixels,
+                        const float centerX, const float centerY,
+                        const float scale,  // length per pixel
+                        const int WIDTH, const int HEIGHT);
+
 
 
 int main(int argc, const char *argv[]) {
 
-    const unsigned WIDTH = 2000;
-    const unsigned HEIGHT = 1200;
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
 
-    MANDELBROT_HEIGHT = MANDELBROT_WIDTH * HEIGHT / WIDTH;
+    const float MANDELBROT_WIDTH  = 3.0;
+    float scale = MANDELBROT_WIDTH / WIDTH;
+    float centerX = -0.5, centerY = 0;  // coordinated of the center point on screen
+
+    mouseState_t mouseState = {{0,0}, false};
 
     bool test_mode = argc > 1;
     int  test_count = 0;
@@ -34,17 +45,17 @@ int main(int argc, const char *argv[]) {
 
     if (test_mode) {
         for (; total_rendered < test_count; total_rendered++) {
-            calculateMandelbrot(screen, WIDTH, HEIGHT, 1.0);
+            calculateMandelbrot(screen, centerX, centerY, scale, WIDTH, HEIGHT);
         }
         free(screen);
         return 0;
     }
 
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot");
+
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot"); // creating window
 
 
-
-    sf::Texture mandelbrot;
+    sf::Texture mandelbrot;             // screen texture
     mandelbrot.create(WIDTH, HEIGHT);
 
     sf::Sprite  mandelbrot_sprite;
@@ -54,14 +65,57 @@ int main(int argc, const char *argv[]) {
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event) ) {
-            if (event.type ==sf::Event::Closed) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
                 break;
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    mouseState.pressed = true;
+                    mouseState.oldPosition.x = event.mouseButton.x;
+                    mouseState.oldPosition.y = event.mouseButton.y;
+                }
+            } else
+            if (event.type == sf::Event::MouseButtonReleased) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    mouseState.pressed = false;
+                }
+            } else
+            if (event.type == sf::Event::MouseMoved) {
+                if (mouseState.pressed) {
+                    centerX -= (event.mouseMove.x - mouseState.oldPosition.x) * scale;
+                    centerY += (event.mouseMove.y - mouseState.oldPosition.y) * scale;
+                    mouseState.oldPosition.x = event.mouseMove.x;
+                    mouseState.oldPosition.y = event.mouseMove.y;
+                }
+            } else
+            if (event.type == sf::Event::MouseWheelScrolled) {
+                if (event.mouseWheelScroll.delta > 0) {
+                    scale *= 0.8 * event.mouseWheelScroll.delta;
+                } else {
+                    scale *= -(1.0/0.8) * event.mouseWheelScroll.delta;
+                }
+            } else
+            if (event.type == sf::Event::KeyPressed) {
+                switch(event.key.code) {
+                    case sf::Keyboard::Up:
+                        scale *= 0.8;
+                        break;
+                    case sf::Keyboard::Down:
+                        scale *= 1.0/0.8;
+                        break;
+                    case sf::Keyboard::Escape:
+                        window.close();
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 
 
-        calculateMandelbrot(screen, WIDTH, HEIGHT, 1.0);
+        calculateMandelbrot(screen, centerX, centerY, scale, WIDTH, HEIGHT);
 
         window.clear();
         fprintf(stderr, "1Calc");
@@ -84,16 +138,20 @@ int main(int argc, const char *argv[]) {
 }
 
 
-int calculateMandelbrot(uint32_t *pixels, const unsigned WIDTH, const unsigned HEIGHT, const float scale) {
-    const uint32_t MAX_ITER = 256;
+int calculateMandelbrot(uint32_t *pixels,
+                        const float centerX, const float centerY,
+                        const float scale,
+                        const int WIDTH, const int HEIGHT) {
+
+    const int MAX_ITER = 256;
     const float ESCAPE_RADIUS2 = 100.f;
 
-    for (unsigned iy = 0; iy < HEIGHT; iy++) {
-        float x0 = -2 * scale,
-              y0 = (MANDELBROT_HEIGHT / 2 - (float)(iy) / HEIGHT * MANDELBROT_HEIGHT) * scale;
+    for (int iy = 0; iy < HEIGHT; iy++) {
+        float x0 = centerX - scale * WIDTH / 2,
+              y0 = centerY + (HEIGHT/2 - iy) * scale;
+        float dx = scale;
 
-        float dx = MANDELBROT_WIDTH / WIDTH * scale;
-        for (unsigned ix = 0; ix < WIDTH; ix++, x0 += dx) {
+        for (int ix = 0; ix < WIDTH; ix++, x0 += dx) {
             /*
                 z' = z^2 + z_0 = (x+iy)^2 + x_0 + iy_0 = x^2 - y^2 + x_0 + i(2x*y + y_0)
                 x' = x^2 - y^2 + x0
@@ -101,7 +159,7 @@ int calculateMandelbrot(uint32_t *pixels, const unsigned WIDTH, const unsigned H
             */
             float x = x0, y = y0;
 
-            uint32_t iter = 0;
+            int iter = 0;
             for (; iter < MAX_ITER; iter++) {
                 float x2 = x*x, y2 = y*y, xy_2 = 2*x*y;
                 if (x2 + y2 > ESCAPE_RADIUS2)
